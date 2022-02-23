b6a3b482c531f4c287071b3297edfe1d431c2ba2
Rapkowsky <robertrapkowski19@gmail.com>
2022-02-23 22:10:32 +0100

    startStopWatch function added

--- js/main.js
+++ js/main.js
@@ -10,3 +10,25 @@ const timeList = document.querySelector(".time-list");
 const infoBtn = document.querySelector(".info");
 const modalShadow = document.querySelector(".modal-shadow");
 const closeModalBtn = document.querySelector(".close");
+let countTime;
+let minutes = 0;
+let seconds = 0;
+
+const startStopWatch = () => {
+	countTime = setInterval(() => {
+		if (seconds < 9) {
+			seconds++;
+			stopwatch.textContent = `${minutes}:0${seconds}`;
+			console.log(seconds);
+		} else if (seconds >= 9 && seconds < 59) {
+			seconds++;
+			stopwatch.textContent = `${minutes}:${seconds}`;
+		} else {
+			minutes++;
+			seconds = 0;
+			stopwatch.textContent = `${minutes}:0`;
+		}
+	}, 100);
+};
+
+startBtn.addEventListener("click", startStopWatch);
